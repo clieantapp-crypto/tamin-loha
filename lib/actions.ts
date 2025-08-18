885ad7fad461bffe38b504e80cb20ede63658d70
clieantapp-crypto <clieantapp@gmail.com>
2025-08-18 14:35:16 +0300

    f

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,5 @@
 export const playNotificationSound = () => {
-  const audio = new Audio("/npt.mp3");
+  const audio = new Audio("/notification-18-270129.mp3");
   if (audio) {
     audio!.play().catch((error) => {
       console.error("Failed to play sound:", error);
